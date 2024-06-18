31b58205c8554c7579ce153aacbd8fd7c581542e
Piotr Ginał <70772418+piotr-ginal@users.noreply.github.com>
2024-06-18 17:28:36 +0200

    refactor: Refactor util package

--- src/tmmail/util/api_request.py
+++ src/tmmail/util/api_request.py
@@ -2,30 +2,43 @@ from urllib.parse import urljoin
 
 import requests
 
-from .. import BASE_API
-from ..exceptions import auth as auth_exceptions
-from ..exceptions import exceptions
+from tmmail import BASE_API
+from tmmail.exceptions import auth as auth_exceptions
+from tmmail.exceptions import exceptions
+
+TOO_MANY_REQUESTS_EXPECTED_STATUS_CODE = 429
+UNAUTHORIZED_STATUS_CODE = 401
 
 
-def api_request(method: str, endpoint: str, **kwargs) -> requests.Response:
-
+def api_request(
+    method: str,
+    endpoint: str,
+    timeout: int = 3,
+    **kwargs,  # noqa: ANN003
+) -> requests.Response:
     request_url = urljoin(BASE_API, endpoint)
 
-    response = requests.request(method, request_url, **kwargs)
+    response = requests.request(method, request_url, timeout=timeout, **kwargs)
 
-    if response.status_code == 429:
-        raise exceptions.TooManyRequestsException()
+    if response.status_code == TOO_MANY_REQUESTS_EXPECTED_STATUS_CODE:
+        raise exceptions.TooManyRequestsException
 
-    elif response.status_code == 401:
-        raise auth_exceptions.UnauthorizedException()
+    if response.status_code == UNAUTHORIZED_STATUS_CODE:
+        raise auth_exceptions.UnauthorizedException
 
     return response
 
 
-def authenticated_api_request(jwt_token: str, method: str, endpoint: str, **kwargs) -> requests.Response:
+def authenticated_api_request(
+    jwt_token: str,
+    method: str,
+    endpoint: str,
+    timeout: int = 3,
+    **kwargs,  # noqa: ANN003
+) -> requests.Response:
     if "headers" not in kwargs:
         kwargs["headers"] = {}
 
     kwargs["headers"]["Authorization"] = f"Bearer {jwt_token}"
 
-    return api_request(method, endpoint, **kwargs)
+    return api_request(method, endpoint, timeout=timeout,**kwargs)
